259c866905f6e8886a8b65e3dd52a548caae8917
Tad-z <david.akintade1000@gmail.com>
2023-08-16 14:30:29 +0100

    push

--- LinkedList.js
+++ LinkedList.js
@@ -1,5 +1,11 @@
 10-- > 5-- > 16
 
+class Node {
+    constructor(value) {
+        this.value = value,
+        this.next = null
+    }
+}
 class LinkedList {
     constructor(value) {
         this.head = {
@@ -10,27 +16,70 @@ class LinkedList {
         this.length = 1;
     }
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
         return this;
     }
     prepend(value) {
-        const headNode = {
-            value: value,
-            next: this.head
-        }
-        this.head = headNode;
+        const newNode = new Node(value)
+        newNode.next = this.head;
+        this.head = newNode;
         this.length++;
         return this
+    }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+    insert(index, value) {
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+        }
+        if(index >= this.length) {
+            return this.append(value)
+        }
+        const newNode = new Node(value);
+        let prevNode = this.head
+        for(let i = 0; i < index-1; i++) {
+            prevNode = prevNode.next
+        }
+        let nextNode = prevNode.next;  
+        prevNode.next = newNode
+        newNode.next = nextNode
+        this.length++
+        return myLinkedList.printList()
+    }
+    remove(index) {
+        if (index === 0) {
+            let nextNode = this.head.next;
+            this.head.next = null
+            this.head = nextNode
+            return myLinkedList.printList()
+        }
+        let prevNode = this.head
+        for(let i = 0; i < index-1; i++) {
+            prevNode = prevNode.next
+        }
+        let targetNode = prevNode.next
+        let nextNode = targetNode.next
+        prevNode.next = nextNode;
+        targetNode.next = null;
+        this.length--;
+        return myLinkedList.printList()
     }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
-myLinkedList.prepend(2)
+myLinkedList.prepend(8)
+myLinkedList.insert(1, 20)
+myLinkedList.remove(0)
