9ffe34fb519839ba655fc5a2c2bd4ff48a7c4235
Tad-z <david.akintade1000@gmail.com>
2023-08-21 14:17:17 +0100

    push

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-// const nemo = ["nemo"];
-// const everyone = ["billy", "mandy", "ben", "emma", "abdul", "kwest", "david", "nemo"]
+const nemo = ["nemo"];
+const everyone = ["billy", "mandy", "ben", "emma", "abdul", "kwest", "david", "nemo"]
 
-// function findNemo(array) {
-//     let t0 = performance.now()
-//     for(i=0; i < array.length; i++) {
-//         if (array[i] === 'nemo') {
-//             console.log("found nemo");
-//         }
-//     }
-//     let t1 = performance.now()
-//     console.log(t1-t0 + "milliseconds");
-// }
+function findNemo(array) {
+    let t0 = performance.now()
+    for(i=0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log("found nemo");
+        }
+    }
+    let t1 = performance.now()
+    console.log(t1-t0 + "milliseconds");
+}
 
 // findNemo(everyone);
 
-// const boxes = [1,2,3,4,5];
+const boxes = [1,2,3,4,5];
 
-// function pairBoxes(array) {
-//     for(let i = 0; i < array.length; i++) {
-//         for(j = 0; j < array.length; j++){
-//             console.log(array[i],array[j]);
-//         }
-//     }
-// }
+function pairBoxes(array) {
+    for(let i = 0; i < array.length; i++) {
+        for(j = 0; j < array.length; j++){
+            console.log(array[i],array[j]);
+        }
+    }
+}
 
 // pairBoxes(boxes);
 
-// function printAndSum(numbers) {
-//     console.log("numbers:")
-//     numbers.forEach(number => {
-//         console.log(number);
-//     }); 
+function printAndSum(numbers) {
+    console.log("numbers:")
+    numbers.forEach(number => {
+        console.log(number);
+    }); 
 
-//     console.log("sums:");
-//     numbers.forEach(firstNumber => {
-//         numbers.forEach(secondNumber => {
-//             console.log(firstNumber + secondNumber);
-//         })
-//     })
-// }
+    console.log("sums:");
+    numbers.forEach(firstNumber => {
+        numbers.forEach(secondNumber => {
+            console.log(firstNumber + secondNumber);
+        })
+    })
+}
 
 // printAndSum([1,2,3,4,5]) 
 
