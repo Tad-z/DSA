8509350f236cb0c051ebe3927a06449fcd3e7b10
Tad-z <david.akintade1000@gmail.com>
2023-12-14 16:39:21 +0100

    push

--- codeWars.py
+++ codeWars.py
@@ -5,3 +5,21 @@ def get_count(sentence):
             count += 1
             
     return count
+
+# Write a function that takes in a string of one or more words,
+#  and returns the same string, but with all five or more letter words reversed
+#   (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. 
+#   Spaces will be included only when more than one word is present.
+
+def spin_words(sentence):
+    array = sentence.split()
+    reverse_array = []
+    for element in array:
+        if len(element) >= 5:
+            reverse = element[::-1]
+            reverse_array.append(reverse)
+        else:
+            reverse_array.append(element)
+
+    new_sentence = ' '.join(reverse_array)
+    return new_sentence
--- leetcode.js
+++ leetcode.js
@@ -1,3 +1,6 @@
+// return true for numbers like 121, 131 becuase they are the same backwards and forward
+//  false for numbers like 20, 30 e.t.c
+
 function isPalindrome (x) {
     let s = x.toString();
     let array = []
@@ -7,4 +10,32 @@ function isPalindrome (x) {
     }
     const reverse = parseInt(array.join(""))
     return x === reverse
+};
+
+// An algorithm to convert roman numerals to integers
+function romanToInt(s) {
+    const conversion = {
+        'I': 1,
+        'V': 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    let sum = 0
+    for (let i = 0; i < s.length; i++) {
+        let current = conversion[s[i]];
+        let next = conversion[s[i+1]]
+
+        if (next && next > current) {
+            sum += next - current
+            i++
+        } else {
+            sum += current
+        }
+    }
+
+    return sum
 };
