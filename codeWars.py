c03caccc53c35cca9307f97767f025fcca6d31c3
Tad-z <david.akintade1000@gmail.com>
2023-12-17 16:23:55 +0100

    push

--- codeWars.py
+++ codeWars.py
@@ -39,3 +39,17 @@ import re
 
 def disemvowel(string_):
     return re.sub(r'[aeiouAEIOU]', '', string_)
+
+
+# Implement the function unique_in_order which takes as argument a sequence 
+# and returns a list of items without any elements with the same value next 
+# to each other and preserving the original order of elements.
+
+def unique_in_order(sequence):
+    array = []
+    for i in range(len(sequence)):
+        prev = i - 1
+        if i == 0 or sequence[i] != sequence[prev]:
+            array.append(sequence[i])
+
+    return array
--- leetcode.js
+++ leetcode.js
@@ -77,3 +77,116 @@ function longestCommonPrefix(strings) {
 const array_of_strings = ["flower", "flow", "flight"];
 const result = longestCommonPrefix(array_of_strings);
 console.log(result);
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+function isValid(s) {
+    const stack = [];
+    const brackets = { 
+        '(': ')', 
+        '[': ']', 
+        '{': '}' 
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const currentBracket = s[i];
+
+        if (currentBracket in brackets) {
+            // It's an opening bracket, push to the stack
+            stack.push(currentBracket);
+        } else {
+            // It's a closing bracket
+            const lastOpenBracket = stack.pop();
+
+            // Check if the corresponding opening bracket matches
+            if (brackets[lastOpenBracket] !== currentBracket) {
+                return false;
+            }
+        }
+    }
+
+    // The stack should be empty if all brackets are properly closed
+    return stack.length === 0;
+}
+
+
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(list1, list2) {
+   const dummy = new ListNode(0)
+   let current = dummy
+
+   console.log(list1)
+   while (list1 != null && list2 !== null) {
+       if(list1.val < list2.val) {
+           current.next = list1
+           list1 = list1.next
+       } else {
+           current.next = list2
+           list2 = list2.next
+       }
+       current = current.next
+   }
+
+   if(list1 !== null) {
+       current.next = list1
+   } else {
+       current.next = list2
+   }
+
+   return dummy.next
+}
+
+// Helper function to create a linked list from an array
+// function arrayToList(arr) {
+//     let dummy = new ListNode(-1);
+//     let current = dummy;
+//     for (let val of arr) {
+//         current.next = new ListNode(val);
+//         current = current.next;
+//     }
+//     return dummy.next;
+// }
+
+// Helper function to convert a linked list to an array
+// function listToArray(head) {
+//     let result = [];
+//     while (head !== null) {
+//         result.push(head.val);
+//         head = head.next;
+//     }
+//     return result;
+// }
+
+// const list1 = arrayToList([1, 2, 4]);
+// const list2 = arrayToList([1, 3, 4]);
+
+// const mergedList = mergeTwoLists(list1, list2);
+// console.log(listToArray(mergedList));
+
+
+
