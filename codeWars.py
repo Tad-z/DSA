d717ca67f8e1385f2edeaacb458489f165551639
Tad-z <david.akintade1000@gmail.com>
2023-12-18 19:23:26 +0100

    push

--- codeWars.py
+++ codeWars.py
@@ -53,3 +53,25 @@ def unique_in_order(sequence):
             array.append(sequence[i])
 
     return array
+
+# The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+# They would like your help with an application form that will tell prospective members which category they will be placed.
+
+# To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+# In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+def open_or_senior(data):
+    output = []
+    for element in data:
+        age = element[0]
+        handicap = element[1]
+        if age >= 55 and handicap > 7:
+            output.append("Senior")
+        else:
+            output.append("Open")
+    return output
+
+# or
+
+def openOrSenior(data):
+  return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
