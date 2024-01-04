3c46c6767a6f81c938bf808308b73232a61fb30c
Tad-z <david.akintade1000@gmail.com>
2024-01-04 23:01:30 +0100

    leetcoding

--- leetcode.js
+++ leetcode.js
@@ -533,4 +533,32 @@ var longestPalindrome = function(s) {
 
 console.log(longestPalindrome("babad"))
 
+// Given a signed 32-bit integer x, return x with its digits reversed. 
+
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+var reverse = function(x) {
+    const INT_MAX = Math.pow(2, 31) - 1;
+    let res = 0;
+    let flag = x < 0 ? -1 : 1;
+
+    x *= flag;
+
+    while (x != 0) {
+        let rem = x % 10;
+        x = Math.floor(x / 10);
+
+
+        if (res > (INT_MAX - rem) / 10) {
+            return 0;
+        }
+
+        res = res * 10 + rem;
+    }
+
+    return flag * res;
+    
+};
+
 
