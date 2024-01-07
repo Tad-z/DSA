9801a74c79d965e52c306db1ba68fdc601e401f9
Tad-z <david.akintade1000@gmail.com>
2024-01-07 22:42:33 +0100

    leetcoding

--- leetcode.js
+++ leetcode.js
@@ -561,4 +561,31 @@ var reverse = function(x) {
     
 };
 
+var intToRoman = function (num) {
+    let str = "";
+    const conversion = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+
+
+    for (key in conversion) {
+        str += key.repeat(Math.floor(num / conversion[key]));
+        num = num % conversion[key];
+    }
+
+    return str
+};
+
 
