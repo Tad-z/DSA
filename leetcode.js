074ecadf06bfb3069041d44b85bdee54a3b4c076
Tad-z <david.akintade1000@gmail.com>
2024-01-12 19:10:57 +0100

    leetcoding

--- leetcode.js
+++ leetcode.js
@@ -588,4 +588,56 @@ var intToRoman = function (num) {
     return str
 };
 
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
+
+// i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+var threeSum = function (nums) {
+    let result = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicate values for the first element
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+        let target = -nums[i]
+
+        while (left < right) {
+            let sum = nums[left] + nums[right]
+            if (sum === target) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicate values for the second element
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++
+                }
+                // Skip duplicate values for the third element
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--
+                }
+
+
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return result
+};
+
 
