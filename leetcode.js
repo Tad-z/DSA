79682271c7a7a0a52fd5114f5d0fc92ce9b86302
Tad-z <david.akintade1000@gmail.com>
2024-01-15 22:39:58 +0100

    leetcoding

--- leetcode.js
+++ leetcode.js
@@ -680,4 +680,41 @@ var threeSumClosest = function(nums, target) {
     return closest
 };
 
+var letterCombinations = function(digits) {
+    if (!digits) {
+        return [];
+    }
+    
+    const map = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
+
+    function backtrack(index, path) {
+        if (index === digits.length) {
+            combinations.push(path.join(''))
+            return
+        }
+
+        let letters = map[digits[index]];
+
+        for (let letter of letters) {
+            path.push(letter)
+            backtrack(index + 1, path)
+            path.pop()
+        }
+    }
+
+    let combinations = []
+    backtrack(0, [])
+    return combinations
+};
+
+
 
