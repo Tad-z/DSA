77546829b49693a12ca49b921644c8f33939154e
Tad-z <david.akintade1000@gmail.com>
2024-01-23 19:54:07 +0100

    leetcoding

--- leetcode.js
+++ leetcode.js
@@ -802,6 +802,7 @@ var rob = function(nums) {
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
 var maxProfit = function (prices) {
     let maxProfit = 0;
     let cheapest = prices[0];
