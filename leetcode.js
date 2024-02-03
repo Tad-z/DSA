503258f20ffc8b49c75ffc695ced0c0733f32539
Tad-z <david.akintade1000@gmail.com>
2024-02-03 23:17:07 +0100

    push

--- leetcode.js
+++ leetcode.js
@@ -882,3 +882,22 @@ var reverseString = function(s) {
     return s.reverse()
 };
 
+
+var fizzBuzz = function(n) {
+    let answer = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            answer.push("FizzBuzz")
+        } else if (i % 3 == 0) {
+            answer.push("Fizz")
+        } else if (i % 5 == 0) {
+            answer.push("Buzz")
+        }
+        else {
+            s = 
+            answer.push(i.toString())
+        }
+    }
+    return answer
+};
+
