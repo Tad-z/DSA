be317d365c09f543160d9ab0faa501f448d47878
Tad-z <david.akintade1000@gmail.com>
2024-02-06 20:26:34 +0100

    push

--- leetcode2.js
+++ leetcode2.js
@@ -55,17 +55,5 @@ var moveZeroes = function(nums) {
 
 // or
 
-var moveZeroes = function(nums) {
-    let nonZeroCount = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[nonZeroCount] = nums[i];
-            nonZeroCount++;
-        }
-    }
 
-    for (let i = nonZeroCount; i < nums.length; i++) {
-        nums[i] = 0;
-    }
-};
 
