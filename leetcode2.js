a8ccb0b85f63fcf981d33f98d6ba10ec0d25f349
Tad-z <david.akintade1000@gmail.com>
2024-02-06 20:15:55 +0100

    push

--- leetcode2.js
+++ leetcode2.js
@@ -33,3 +33,23 @@ var maxDepth = function(root) {
     return result;
 };
 
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+var moveZeroes = function(nums) {
+    if (nums.length == 1) {
+        return nums;
+    }
+    let p1 = 0;
+    let p2 = 0;
+    while (p1 < nums.length && p2 < nums.length) {
+        if (nums[p2] != 0) {
+            let temp = nums[p1];
+            nums[p1] = nums[p2];
+            nums[p2] = temp;
+            p1++
+        }
+        p2++
+    }
+    return nums
+};
+
