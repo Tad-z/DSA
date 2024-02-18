c69b4ccdd702d78c28873b3bce183dc79d42485f
Tad-z <david.akintade1000@gmail.com>
2024-02-18 09:02:08 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -84,6 +84,19 @@ var reverseList = function(head) {
     return prev;
 };
 
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, 
+// return its corresponding column number.
+
+// For example:
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28 
+
 var titleToNumber = function(s) {
     let conversion = {
         A: 1,
