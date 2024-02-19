8f8464bfcbe9faf5564ec2d17e3b3e37759e3274
Tad-z <david.akintade1000@gmail.com>
2024-02-19 17:04:25 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -150,6 +150,12 @@ var titleToNumber = function (columnTitle) {
     return result;
 };
 
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. 
+
+// You may assume that the majority element always exists in the array.
 var majorityElement = function(nums) {
     let tracker = 0;
     let currElement = -1;
