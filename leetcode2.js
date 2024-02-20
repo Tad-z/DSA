5c47dd310d8e00c2637405d9a28fb4393f734ba6
Tad-z <david.akintade1000@gmail.com>
2024-02-20 21:32:33 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -177,6 +177,11 @@ var majorityElement = function (nums) {
 };
 
 
+// There is a singly-linked list head and we want to delete a node node in it.
+
+// You are given the node to be deleted node. You will not be given access to the first node of head.
+
+// All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
