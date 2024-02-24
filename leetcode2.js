65a17aa582b9f6ee00a3402e2955c32e64ac1f64
Tad-z <david.akintade1000@gmail.com>
2024-02-24 20:51:11 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -213,6 +213,9 @@ var maxProfit = function(prices) {
     return maxProfit;
 };
 
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 var isAnagram = function(s, t) {
     if (s.length !== t.length) {
        return false;
