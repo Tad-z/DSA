1e70f5706d13f5a6bbc0326ef6a0a5da96d787f4
Tad-z <david.akintade1000@gmail.com>
2024-02-28 22:21:39 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -253,7 +253,7 @@ var containsDuplicate = function (nums) {
     return false
 };
 
-
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 var firstUniqChar = function (s) {
     let map = {};
     for (let i = 0; i < s.length; i++) {
