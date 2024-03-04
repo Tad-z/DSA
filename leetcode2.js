2f18c0b90936d3d08f54c3254bd4246629f5fabd
Tad-z <david.akintade1000@gmail.com>
2024-03-04 23:55:52 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -307,3 +307,25 @@ var missingNumber = function(nums) {
     }
     return n    
 };
+
+var intersect = function(nums1, nums2) {
+    let intersection = []
+    nums1 = nums1.sort((a, b) => a - b)
+    nums2 = nums2.sort((a, b) => a - b)
+    let i = 0;
+    let j = 0;
+
+    while (i < nums1.length && j < nums2.length ) {
+        if (nums1[i] === nums2[j]) {
+            intersection.push(nums1[i])
+            i += 1
+            j += 1
+        } else if (nums1[i] < nums2[j]) {
+            i += 1
+        } else {
+            j += 1
+        }
+    }
+
+    return intersection;
+};
