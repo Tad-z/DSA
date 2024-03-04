c6845ad8967458d4f9877936aec8f95b779590c7
Tad-z <david.akintade1000@gmail.com>
2024-03-04 23:56:19 +0100

    dsa

--- leetcode2.js
+++ leetcode2.js
@@ -308,6 +308,8 @@ var missingNumber = function(nums) {
     return n    
 };
 
+// Given two integer arrays nums1 and nums2, return an array of their intersection. 
+// Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 var intersect = function(nums1, nums2) {
     let intersection = []
     nums1 = nums1.sort((a, b) => a - b)
